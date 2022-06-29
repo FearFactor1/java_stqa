c600b70fb590851bf27ea9a01d718cae7bb0fec9
FearFactor1 <Septor6>
2022-06-29 12:01:27 +0300

    Задание №2

--- sandbox/src/main/java/src/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/src/ru/stqa/pft/sandbox/Point.java
@@ -1,15 +1,22 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-    public double p1;
-    public double p2;
 
-    public Point(double p1, double p2) {
-        this.p1 = p1;
-        this.p2 = p2;
+    public double x1;
+    public double y1;
+    public double x2;
+    public double y2;
+
+
+    public Point (double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 
-    public double distance(){
-        return this.p1 - this.p2;
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p2.y2 - p1.y1)*(p2.y2 - p1.y1));
     }
+
 }
--- sandbox/src/main/java/src/ru/stqa/pft/sandbox/RunPoint.java
+++ sandbox/src/main/java/src/ru/stqa/pft/sandbox/RunPoint.java
@@ -1,8 +1,19 @@
 package ru.stqa.pft.sandbox;
 
+import static ru.stqa.pft.sandbox.Point.distance;
+
+
 public class RunPoint {
     public static void main(String[] args) {
-        Point p = new Point(7, 5);
-        System.out.println("расстояния между точками " + p.p1 + " и " + p.p2 + " = " + p.distance());
+        Point p1 = new Point(5, 6, 8,2);
+        Point p2 = new Point(8, 1, 5,2);
+
+        System.out.println("Расстояние между точками (" + p1 + ") и (" + p2 + ") = " + distance(p1, p2));
     }
 }
+
+
+
+
+
+
