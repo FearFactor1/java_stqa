e2d55fa29871937100af370a3dde7bed210042b7
FearFactor1 <Septor6>
2022-07-05 10:58:28 +0300

    Задание №5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupHelper extends HelperBase{
@@ -14,14 +15,36 @@ public class GroupHelper extends HelperBase{
         click(By.linkText("group page"));
     }
 
+    public void returnToContactPage() {
+        click(By.linkText("home page"));
+    }
+
     public void submitGroupCreation() {
         click(By.name("submit"));
+    }
+
+    public void submitContactCreation() {
+        wd.findElement(By.name("submit")).click();
     }
 
     public void fillGroupForm(GroupData groupData) {
         type(By.name("group_name"), groupData.getName());
         type(By.name("group_header"), groupData.getHeader());
         type(By.name("group_footer"), groupData.getFooter());
+    }
+
+    public void fillContactForm(ContactData contactData) {
+        type(By.name("lastname"), contactData.getLastname());
+        type(By.name("firstname"), contactData.getFirstname());
+        type(By.name("address"), contactData.getAddress());
+        type(By.name("email"), contactData.getAllEmail());
+        type(By.name("home"), contactData.getAllPhones());
+    }
+
+    private void surname(ContactData contactData) {
+        click(By.name("lastname"));
+        clear(By.name("lastname"));
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     }
 
     public void initGroupCreation() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationsTests.java
@@ -1,103 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.time.Duration;
 
-public class ContactCreationsTests {
-    private WebDriver wd;
-
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "F:\\IdeaProjects\\java_stqa\\chromedriver.exe");
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-    }
+public class ContactCreationsTests extends TestBase {
 
     @Test
     public void testContactCreations() throws Exception {
-        login("admin", "secret");
-        gotoContactPage();
-        fillContactForm(new ContactData("Ivanov", "Dmitriy",
-                "г. Тамбов, ул. Лесная, д.35, кв. 65", "bugagvbm@yandex.ru", "+79253478323"));
-        submitContactCreation();
-        returnToContactPage();
-        logout();
+        app.getNavigationHelper().gotoContactPage();
+        app.getGroupHelper().fillContactForm(new ContactData(
+                "Petrov", "Vasek", "Г. Саратов, ул. Озёрная, д.45, кв. 23",
+                "ferdcvb@yandex.ru", "+79253478354"));
+        app.getGroupHelper().submitContactCreation();
+        app.getGroupHelper().returnToContactPage();
+        app.getSessionHelper().logout();
     }
 
-    private void logout() {
-        wd.findElement(By.linkText("Logout")).click();
-    }
-
-    private void returnToContactPage() {
-        wd.findElement(By.linkText("home page")).click();
-    }
-
-    private void submitContactCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    private void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getAllEmail());
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(contactData.getAllPhones());
-    }
-
-    private void gotoContactPage() {
-        wd.findElement(By.linkText("add new")).click();
-    }
-
-    private void login(String username, String password) {
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        wd.quit();
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 
 }
