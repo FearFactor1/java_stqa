f1ab26777415d9fa34155f26f4d8a50f9d54b050
FearFactor1 <Septor6>
2022-08-08 12:03:41 +0300

    Лекция 7.3. Условия отбора данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,47 +3,64 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import jakarta.persistence.*;
 
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
     @XStreamOmitField
+    @Id
+    @Column(name = "id")
     private int id = Integer.MAX_VALUE;
     @Expose
+    @Column(name = "lastname")
     private String lastname;
     @Expose
+    @Column(name = "firstname")
     private String firstname;
     @Expose
+    @Column(name = "address")
     private String address;
     @Expose
+    @Column(name = "email")
     private String allEmail;
     @Expose
+    @Column(name = "email2")
     private String allEmail2;
     @Expose
+    @Column(name = "email3")
     private String allEmail3;
     @Expose
+    @Transient
     private String allPhones;
     @Expose
+    @Transient
     private String group;
     @Expose
+    @Column(name = "home")
     private String homePhone;
     @Expose
+    @Column(name = "mobile")
     private String mobile;
     @Expose
+    @Column(name = "work")
     private String workPhone;
+    @Expose
+    @Column(name = "photo")
+    private String photo;
 
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
-
-    private File photo;
 
     public int getId() {
         return id;
