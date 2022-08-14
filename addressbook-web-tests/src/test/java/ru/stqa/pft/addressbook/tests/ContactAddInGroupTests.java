7820e71c40a5eb8f203972c4aedd4e8305a466cf
FearFactor1 <Septor6>
2022-08-14 19:12:19 +0300

    Задание №16

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddInGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddInGroupTests.java
@@ -1,44 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
-import ru.stqa.pft.addressbook.model.Groups;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactAddInGroupTests extends TestBase {
-
-    @DataProvider
-    public Iterator<Object[]> validContactsFromJson() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File(
-                "src/test/resources/contacts.json")))) {
-            String json = "";
-            String line = reader.readLine();
-            while (line != null) {
-                json += line;
-                line = reader.readLine();
-            }
-            Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
-            List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
-            return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
-        }
-    }
 
     @BeforeMethod
     public void ensurePreconditions() throws IOException {
@@ -54,11 +27,10 @@ public class ContactAddInGroupTests extends TestBase {
         }
     }
 
-    @Test(dataProvider = "validContactsFromJson")
-    public void testContactAddInGroup(ContactData contact) throws Exception {
-        Groups groups = app.db().groups();
+    @Test
+    public void testContactAddInGroup() throws Exception {
         Contacts before = app.db().contacts();
-        app.contact().addInGroup(before.iterator().next(), groups.iterator().next());
+        app.contact().addContactInGroup(before.iterator().next());
         Contacts after = app.db().contacts();
         assertThat(after.size(),  equalTo(before.size() + 1));
     }
