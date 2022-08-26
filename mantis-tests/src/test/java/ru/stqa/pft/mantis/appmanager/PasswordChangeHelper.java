7e49cbf538b83c96c848150ba0b977c5bdf1f8ef
FearFactor1 <Septor6>
2022-08-26 17:47:47 +0300

    Задание №18

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/PasswordChangeHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/PasswordChangeHelper.java
@@ -1,8 +1,11 @@
 package ru.stqa.pft.mantis.appmanager;
 
 import org.openqa.selenium.By;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
 
 import javax.mail.MessagingException;
+import java.util.List;
 
 public class PasswordChangeHelper extends HelperBase {
 
@@ -13,9 +16,9 @@ public class PasswordChangeHelper extends HelperBase {
     public void start(String username, String password) {
         wd.get(app.getProperty("web.baseUrl") + "login_page.php");
         type(By.name("username"), username);
-        click(By.cssSelector("input[value='Вход']"));
+        click(By.cssSelector("input[type='submit']"));
         type(By.name("password"), password);
-        click(By.cssSelector("input[value='Вход']"));
+        click(By.cssSelector("input[type='submit']"));
     }
 
     public void resetPassword(String username) throws MessagingException, InterruptedException {
@@ -24,4 +27,19 @@ public class PasswordChangeHelper extends HelperBase {
         click(By.linkText(username));
         click(By.xpath("//input[@value='Сбросить пароль']"));
     }
+
+    public String findConfLink(List<MailMessage> mailMessages, String mail) {
+        MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(mail)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+    public void changePassInEmailForm(String userName, String confirmationLink, String password) {
+        wd.get(confirmationLink);
+        type(By.id("realname"), userName);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("button[type='submit']"));
+    }
+
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/PasswordChangeTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/PasswordChangeTests.java
@@ -2,21 +2,31 @@ package ru.stqa.pft.mantis.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.model.MailMessage;
+import ru.stqa.pft.mantis.appmanager.HttpSession;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
+import static java.lang.Math.random;
+import static org.testng.Assert.assertTrue;
+
 public class PasswordChangeTests extends TestBase {
 
     @Test
     public void testPasswordChange() throws IOException, MessagingException, InterruptedException {
+        app.mail().start();
         app.passwordChange().start("administrator", "root");
         app.passwordChange().resetPassword("adm");
         long now = System.currentTimeMillis();
-        //String user = String.format("user%s", now);
-        String password = "password";
-        List<MailMessage> mailMessages = app.james().waitForMail("adm", password, 919167000);
-        System.out.println(mailMessages);
+        List<MailMessage> mailMessages = app.mail().waitForMail(1, 1000);
+        String confirmationLink = app.passwordChange().findConfLink(mailMessages, "adm@localhost");
+        String password = "adm" + random();
+        app.passwordChange().changePassInEmailForm("adm",
+                confirmationLink, password);
+        assertTrue(app.newSession().login("adm", password));
     }
+
 }
+
+
